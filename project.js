9e398147ed1254e0519c206b7656f19361e84830
kelivreno <hakim.kelangit@student.greenriver.edu>
2023-03-12 01:40:22 -0800

    installing packages

---
+++ project.js
@@ -0,0 +1,8 @@
+// 1. Deposit some money
+// 2. Determine the number of lines to bet on
+// 3. Collect the bet amount
+// 4. Spin the slot amount
+// 5. Check if the user won
+// 6. Give the user the winnings
+// 7. Play again
+
