a79d747bde549922fccaaa0215a713a380e8fef9
kelivreno <hakim.kelangit@student.greenriver.edu>
2023-03-12 03:02:41 -0700

    added a input function and while loop

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+// STEPS:
 // 1. Deposit some money
 // 2. Determine the number of lines to bet on
 // 3. Collect the bet amount
@@ -6,3 +7,25 @@
 // 6. Give the user the winnings
 // 7. Play again
 
+// Const = constant
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+        // OR = || 
+        if (isNaN(numberDepositAmount) || numberDepositAmount <=0) {
+            console.log("Invalid deposit amount, try again. ");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+// In english, it means, if the statement of numberDepositAmount is NaN and is less than 0
+// It will return the prompt over and over again, unless it does not meet that criteria anymore
+// Therefore, the else is the 
+
+deposit();
