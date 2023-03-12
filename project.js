b914080cf388ae0f108210279c771c64446c401a
kelivreno <hakim.kelangit@student.greenriver.edu>
2023-03-12 13:47:47 -0700

    made getBet() and getLines ()

--- project.js
+++ project.js
@@ -9,6 +9,7 @@
 
 // Const = constant
 
+//  DEPOSIT INPUT START 
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
@@ -28,4 +29,48 @@ const deposit = () => {
 // It will return the prompt over and over again, unless it does not meet that criteria anymore
 // Therefore, the else is the 
 
-deposit();
+// DEPOSIT INPUT END
+
+// NUMBER OF LINES START 
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+        // OR = || 
+        if (isNaN(numberOfLines) || numberOfLines <=0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again. ");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+// NUMBER OF LINES END
+
+// Collect bet amount start
+const getBet = (balance, lines) => {
+    // We can use the balance inside this function 
+    while (true) {
+        const bet = prompt("Enter the total bet: ");
+        const numberBet = parseFloat(bet);
+        // OR = || 
+        if (isNaN(numberBet) || numberBet <=0 || numberBet > balance / lines ) {
+            console.log("Invalid bet, try again. ");
+        } else {
+            return numberBet * lines;
+        }
+    }
+};
+// Collect bet amount end
+
+
+
+let balance = deposit();
+
+// A variable using let, can be altered (added or substracted)
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
+console.log(bet);
+//  It will print out the deposit amount, console log is a print function
+//  It outputs a message to the web console/terminal
